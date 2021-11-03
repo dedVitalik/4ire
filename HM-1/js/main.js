bc2cb7af9e9efdfa69888f90c3447ac920aeb4b9
Dedvitalik <vitaliy.vinnik@gmail.com>
2021-11-03 12:42:19 +0200

    looks like it's finish

--- HM-1/js/main.js
+++ HM-1/js/main.js
@@ -1,27 +1,72 @@
-let currencyToChange = prompt('Яку валюту Ви хочете обміняти? Ви можете скористатися сервісом для обміну: EUR, USD, UAH, GBP, RUR').toLowerCase();
-let rateToChange;
-let amountToReceive;
-let 
+const EUR_RATE = 30.51;
+const USD_RATE = 26.35;
+const UAH_RATE = 1;
+const GBP_RATE = 35.87;
+const RUR_RATE = 0.37;
+let weAreWorking = true;
 
-switch (currencyToChange) {
-  case "eur":
-    console.log("Oranges are $0.59 a pound.");
-    break;
-  case "Apples":
-    console.log("Apples are $0.32 a pound.");
-    break;
-  case "Bananas":
-    console.log("Bananas are $0.48 a pound.");
-    break;
-  case "Cherries":
-    console.log("Cherries are $3.00 a pound.");
-    break;
-  case "Mangoes":
-  case "Papayas":
-    console.log("Mangoes and papayas are $2.79 a pound.");
-    break;
-  default:
-    console.log("Sorry, we are out of " + expr + ".");
-}
+do {
+  const currencyToChange = prompt('Яку валюту Ви хочете обміняти? Ви можете скористатися сервісом для обміну: EUR, USD, UAH, GBP, RUR').toLowerCase();
+  let rateToChange;
+  let amountToChange;
+  let currencyToReceive;
+  let rateToReceive;
+  let resultOfChanging;
+  
+  switch (currencyToChange) {
+    case "eur":
+      rateToChange = EUR_RATE;
+      break;
+    case "usd":
+      rateToChange = USD_RATE;
+      break;
+    case "uah":
+      rateToChange = UAH_RATE;
+      break;
+    case "gbp":
+      rateToChange = GBP_RATE;
+      break;
+    case "rur":
+      rateToChange = RUR_RATE;
+      break;
+    default:
+      alert("Ви не правильно ввели валюту для обміну, введіть будь-ласка EUR, USD, UAH, GBP або RUR");
+      continue;
+  }
+  
+  do {
+    amountToChange = +prompt(`Скільки ${currencyToChange.toUpperCase()} Ви хочете обміняти?`);
+    if (isNaN(amountToChange)) {
+      alert("Введіть числова значення!");
+    };
+  } while (isNaN(amountToChange));
 
-console.log("Is there anything else you'd like?");
+  do {
+    currencyToReceive = prompt(`На яку валюту Ви хочете поміняти ${amountToChange} ${currencyToChange.toUpperCase()}? Введіть будь-ласка EUR, USD, UAH, GBP або RUR`).toLowerCase();
+
+    switch (currencyToReceive) {
+      case "eur":
+        rateToReceive = EUR_RATE;
+        break;
+      case "usd":
+        rateToReceive = USD_RATE;
+        break;
+      case "uah":
+        rateToReceive = UAH_RATE;
+        break;
+      case "gbp":
+        rateToReceive = GBP_RATE;
+        break;
+      case "rur":
+        rateToReceive = RUR_RATE;
+        break;
+      default:
+        alert("Ви не правильно ввели валюту, введіть будь-ласка EUR, USD, UAH, GBP або RUR");
+        continue;
+    }
+  } while (!rateToReceive);
+
+  resultOfChanging = rateToChange*UAH_RATE/rateToReceive*amountToChange;
+  alert(`Ви отримаєте ${resultOfChanging.toFixed(2)}`);
+  weAreWorking = confirm("Проведемо операцію ще раз?");
+} while (weAreWorking);
