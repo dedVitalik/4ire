1d75f3bc2e37ec78b94ad0fb7663ec3b56048fad
Dedvitalik <vitaliy.vinnik@gmail.com>
2021-12-30 02:27:34 +0200

    final hw6

--- hw6/main.js
+++ hw6/main.js
@@ -1,6 +1,8 @@
-// Примітка по верстці. Розміщення елементів в попапі таке як і задумувалося, візуалку підглянув ось тут:
+// Примітки по верстці.
+// 1. Розміщення елементів в попапі таке як і задумувалося, візуалку підглянув ось тут:
 // https://liniakino.com/movies/?id=9592
 // Тобто на полоску з назвою фільма трохи "заїжає" постер
+// 2. Верстка більш менш добре себе почуває від 768 ;)
 
 // утіліта, масив об'єктів з жанрами перетворює в рядок жанрів для виводу на сайті
 const genresToString = (genres) => {
@@ -11,6 +13,7 @@ const genresToString = (genres) => {
   return genresInString.slice(0, -2);
 };
 
+// параметри фетч запиту
 const fetchParams = {
   method: 'GET',
   headers: {
@@ -19,29 +22,45 @@ const fetchParams = {
   },
 };
 
-// функція що робить все ) генератор постерів, попапів, отримувач відповідей з сервера
-function makeFilmCard(film) {
-  const contentBlock = document.querySelector('.main-content'),
-    posterElement = document.createElement('div'),
-    filmPopup = document.querySelector('.film-popup'),
-    posterInfo = `<span class="film-poster__art">${film.genres[0].genre}</span>
-    <img class="film-poster__img" src="${film.posterUrl}" alt="#">
-    <div class="film-poster__info">
-      <time class="film-poster__year">${film.year}</time>
-      <h3 class="film-poster__title">${film.nameRu}</h3>
-      <p class="film-poster__description">${film.description}</p>
-    </div>`;
-  posterElement.classList.add('film-poster');
-  posterElement.innerHTML = posterInfo;
+// масив-база наших фільмів
+let fullFilms;
+// Попап
+const filmPopup = document.querySelector('.film-popup');
 
-  // функція генерації і відображення попапа
-  function showFilmPopup() {
-    const allGenres = genresToString(film.genres);
-    // для, наприклад, російських фільмів немає "оригінальної" назви
-    if (!film.nameOriginal) {
-      film.nameOriginal = film.nameRu;
+// Хендлери
+const popupClosingHandler = function () {
+  filmPopup.classList.add('hidden');
+};
+
+const contentBlockHandler = function (evt) {
+  const isPopupActive = !filmPopup.classList.contains('hidden');
+  let ourTarget = evt.target;
+  if (!ourTarget.classList.contains('main-content')) {
+    while (!ourTarget.classList.contains('film-poster')) {
+      ourTarget = ourTarget.parentNode;
     }
-    const filmInfo = `<h2 class="film-popup__title"><span>${film.nameRu}</span></h2>
+    for (film of fullFilms) {
+      if (ourTarget.querySelector('.film-poster__img').src === film.posterUrl) {
+        if (isPopupActive) {
+          popupClosingHandler();
+        } else {
+          showFilmPopup();
+        }
+      }
+    }
+  } else {
+    popupClosingHandler();
+  }
+};
+
+// функція генерації і відображення попапа
+function showFilmPopup() {
+  const allGenres = genresToString(film.genres);
+  // для, наприклад, російських фільмів немає "оригінальної" назви
+  if (!film.nameOriginal) {
+    film.nameOriginal = film.nameRu;
+  }
+  const filmInfo = `<h2 class="film-popup__title"><span>${film.nameRu}</span></h2>
     <button class="film-popup__closing"></button>
     <img class="film-popup__img" src="${film.posterUrl}" alt="">
       <ul class="film-popup__description-list">
@@ -73,33 +92,31 @@ function makeFilmCard(film) {
           <p class="film-popup__description">${film.description}</p>
         </li>
     </ul>`;
-    filmPopup.innerHTML = filmInfo;
-    filmPopup.classList.remove('hidden'); // робимо попап видимим
-    const popupClosingButton = document.querySelector('.film-popup__closing');
-    popupClosingButton.addEventListener('click', popupClosingHandler); // навішуємо хендлер на клік по закривашці
-  }
-
-  // Хендлери
-  const addPosterHadler = function () {
-    if (filmPopup && !filmPopup.classList.contains('hidden')) {
-      popupClosingHandler();
-    } else {
-      showFilmPopup();
-    }
-  };
-  const popupClosingHandler = function () {
-    filmPopup.classList.add('hidden');
-  };
-
-  // "публікація карточки фільма"
-  posterElement.addEventListener('click', addPosterHadler);
-  contentBlock.insertAdjacentElement('beforeend', posterElement);
+  filmPopup.innerHTML = filmInfo;
+  filmPopup.classList.remove('hidden'); // робимо попап видимим
+  const popupClosingButton = document.querySelector('.film-popup__closing');
+  popupClosingButton.addEventListener('click', popupClosingHandler); // навішуємо хендлер на клік по закривашці
 }
 
-let headers = new Headers();
-headers.append('Content-Type', 'application/json');
-headers.append('X-API-KEY', '1be6749b-dbf1-4f49-814f-8b1bf84b0931');
+const contentBlock = document.querySelector('.main-content');
+contentBlock.addEventListener('click', contentBlockHandler);
 
+// генератор карток фільмів
+function makeFilmCard(film) {
+  const posterElement = document.createElement('div'),
+    posterInfo = `<span class="film-poster__art">${film.genres[0].genre}</span>
+    <img class="film-poster__img" src="${film.posterUrl}" alt="#">
+    <div class="film-poster__info">
+      <time class="film-poster__year">${film.year}</time>
+      <h3 class="film-poster__title">${film.nameRu}</h3>
+      <p class="film-poster__description">${film.description}</p>
+    </div>`;
+  posterElement.classList.add('film-poster');
+  posterElement.innerHTML = posterInfo;
+
+  // "публікація карточки фільма"
+  contentBlock.insertAdjacentElement('beforeend', posterElement);
+}
 // отримуємо дані з сервера
 let promise = fetch(
   'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1',
@@ -108,7 +125,7 @@ let promise = fetch(
 promise
   .then((res) => res.json())
   .then((data) => {
-    let fullFilms = data.films;
+    fullFilms = data.films;
     async function getFilmsDetails(films) {
       for (let i = 0; i < films.length; i++) {
         const filmParsingURI = `https://kinopoiskapiunofficial.tech/api/v2.2/films/${films[i].filmId}`;
